352b92d444e74fd582c2639f4b2a14aa96f1e73c
Keyang <keyang.xiang@feedhenry.com>
2013-11-05 18:52:58 +0000

    more sdk

---
+++ js/06-data.js
@@ -0,0 +1,10 @@
+cms.data=(function(module){
+    module.getContent=getContent;
+    module.getAppStructure=getAppStructure;
+    module.getContentExtra=getContentExtra;
+    module.getRSSFeed=getRSSFeed;
+
+    
+
+    return module;
+})(cms.data || {});
