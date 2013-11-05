dc24b8a7719d96d7b464fe75d30caae9f2f2f9bf
Evan Shortiss <evan.shortiss@feedhenry.com>
2013-11-05 19:56:37 +0000

    Update gitignore

--- dist/cms.js
+++ dist/cms.js
@@ -5,6 +5,10 @@ var cms=(function(module){
         module.alias=alias;
     }
 
+    function setApplication(app){
+        module.app=app;
+    }
+
     return module;
 })(cms ||{});
 
@@ -45,8 +49,11 @@ cms.model=(function(module){
         $fh.act({
             "act":name,
             "req":param
-        },cb,function(err){
+        },function(res){
+            cb(null,res);
+        },function(err){
             console.log(err);
+            cb(err, null);
         });
     }
 
@@ -93,30 +100,214 @@ cms.events=(function(module){
     }
     return module;
 })(cms.events || {});
-cms.model=(function(module){
-    
-    
-    return module;
-})(cms.model ||{});
-cms.service=(function(module){
-    module.sync=sync;// sync content
-    module.startPoll=startPoll; //seconds as parameter
-    module.stopPoll=stopPoll;
+cms.model = (function(module) {
+
+  // Public functions
+  module.create = _create;
+  module.read = _read;
+  module.KEYS = {
+    AppStructure: 'AppStructure'
+  };
 
 
+  /**
+   * Parse JSON to string
+   * @return
+   */
+
+  function stringifyJson(data, callback) {
+    try {
+      return callback(null, JSON.stringify(data))
+    } catch (e) {
+      return callback(e, null);
+    }
+  }
+
+  /**
+   * Parse string to JSON Object
+   */
+
+  function parseToJson(str, callback) {
+    try {
+      return callback(null, JSON.parse(str))
+    } catch (e) {
+      return callback(e, null);
+    }
+  }
 
 
-    return module;
+  /**
+   * Read entry from local storage
+   */
+
+  function _read(key, callback) {
+    $fh.data({
+      act: 'load',
+      key: key
+    }, function(res) {
+      return parseToJson(res.val, callback);
+    }, function(msg, err) {
+      return callback(err, null);
+    });
+  }
+
+
+  /**
+   * Create entry in local storage
+   */
+
+  function _create(key, data, callback) {
+    // Callback is optional
+    callback = callback || function(err) {
+      if(err) {
+        console.log('Failed to save to local storage for key: ' + key, data);
+      }
+    };
+
+    stringifyJson(data, function(err, json) {
+      if (err) {
+        return callback(err, null);
+      }
+
+      $fh.data({
+        act: 'save',
+        key: key,
+        data: json
+      }, function(res) {
+        return callback(null, res);
+      }, function(msg, err) {
+        return callback(err, null);
+      })
+    });
+  }
+
+  return module;
+})(cms.model || {});
+cms.service = (function(module) {
+  module.sync = sync; // sync content
+  module.startPoll = startPoll; //seconds as parameter
+  module.stopPoll = stopPoll;
+
+  // Time when the app should check for an update
+  var timerId = null;
+
+  /**
+   * Get content from CMS and save to localstorage.
+   */
+
+  function sync() {
+    cms.model.getAppStructure(function(err, res) {
+      if(err) {
+        console.log('Failed to get CMS updated content');
+      }
+      console.log('Got updated CMS content.');
+
+      cms.model.create(cms.model.KEYS.AppStructure, res, function(err, res) {
+        if(!err) {
+          console.log('Saved CMS content to localstorage');
+        } else {
+          console.log('Failed to save CMS content to localstorage');
+        }
+      });
+    });
+  }
+
+  // TODO Handle device events, 'resume' etc
+  function startPoll(seconds) {
+    // Clear old timers
+    stopPoll();
+
+    // New timer
+    timerId = setInterval(function() {
+      sync();
+    }, seconds * 1000);
+  }
+
+  function stopPoll() {
+    clearInterval(timerId);
+  }
+
+
+  return module;
 })(cms.service || {});
-cms.data=(function(module){
-    module.getContent=getContent;
-    module.getAppStructure=getAppStructure;
-    module.getContentExtra=getContentExtra;
-    module.getRSSFeed=getRSSFeed;
+cms.data = (function(module) {
+  module.getContent = getContent;
+  module.getAppStructure = getAppStructure;
+  module.getContentExtra = getContentExtra;
+  module.getRSSFeed = getRSSFeed;
 
-    
 
-    return module;
+  function getContent(contentId, callback) {
+    cms.model.getContent(contentId, function(err, res) {
+      if (err) {
+        return cms.model.read(contentId, callback);
+      }
+
+      // Save the content to local storage
+      cms.model.create(contentId, res);
+
+      return callback(null, res);
+    });
+  }
+
+
+  function getAppStructure(callback) {
+    cms.model.getAppStructure(function(err, res) {
+      if (err) {
+        return cms.model.read(cms.model.KEYS.AppStructure, callback);
+      }
+
+      // Save the content to local storage
+      cms.model.create(cms.model.KEYS.AppStructure, res);
+
+      return callback(null, res);
+    });
+  }
+
+
+  function getContentExtra(cat, type, template, extraId, cb) {
+
+    function constructExtraKey(arr) {
+      var key = '';
+      for (var i = arr.length - 1; i >= 0; i--) {
+        key += arr[i];
+      };
+
+      return key;
+    }
+
+    cms.model.getContentExtra(cat, type, template, extraId, function(err, res) {
+      if (err) {
+        return cms.model.read(constructExtraKey([cat, type, template, extraId]), callback);
+      }
+
+      // Save the content to local storage
+      cms.model.create(constructExtraKey([cat, type, template, extraId]), res);
+
+      return callback(null, res);
+    });
+  }
+
+
+  function getRSSFeed(feedId, callback) {
+
+    function constructRssKey(feedId) {
+      return 'rss_' + feedId;
+    }
+
+    cms.model.getRSSFeed(extraId, function(err, res) {
+      if (err) {
+        return cms.model.read(constructRssKey(feedId), callback);
+      }
+
+      // Save the content to local storage
+      cms.model.create(constructRssKey(feedId), res);
+
+      return callback(null, res);
+    });
+  }
+
+  return module;
 })(cms.data || {});
 com.ui=(function(module){
     
