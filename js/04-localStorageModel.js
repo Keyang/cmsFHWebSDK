5898336f79b817b07bcf9691b77089f6c9dd4b4d
Evan Shortiss <evan.shortiss@feedhenry.com>
2013-11-05 18:53:49 +0000

    Add in localstorage wrapper

--- js/04-localStorageModel.js
+++ js/04-localStorageModel.js
@@ -1,5 +1,73 @@
-cms.model=(function(module){
-    
-    
-    return module;
-})(cms.model ||{});
+cms.model = (function(module) {
+
+  // Public functions
+  module.create = _create;
+  module.read = _read;
+
+
+  /**
+   * Parse JSON to string
+   * @return
+   */
+
+  function stringifyJson(data, callback) {
+    try {
+      return callback(null, JSON.stringify(data))
+    } catch (e) {
+      return callback(e, null);
+    }
+  }
+
+  /**
+   * Parse string to JSON Object
+   */
+
+  function parseToJson(str, callback) {
+    try {
+      return callback(null, JSON.parse(str))
+    } catch (e) {
+      return callback(e, null);
+    }
+  }
+
+
+  /**
+   * Read entry from local storage
+   */
+
+  function _read(key, callback) {
+    $fh.data({
+      act: 'load',
+      key: key
+    }, function(res) {
+      return parseToJson(res, callback);
+    }, function(msg, err) {
+      return callback(err, null);
+    });
+  }
+
+
+  /**
+   * Create entry in local storage
+   */
+
+  function _create(key, data, callback) {
+    stringifyJson(data, function(err, json) {
+      if (err) {
+        return callback(err, null);
+      }
+
+      $fh.data({
+        act: 'save',
+        key: key,
+        data: json
+      }, function(res) {
+        return callback(null, res);
+      }, function(msg, err) {
+        return callback(err, null);
+      })
+    });
+  }
+
+  return module;
+})(cms.model || {});
